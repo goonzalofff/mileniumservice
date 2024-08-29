93afc48c4974c128c97c7d3d25853065a8826d53
Gonzalo Coronel <98192857+goonzalofff@users.noreply.github.com>
2024-08-29 19:46:00 -0300

    Update form.js

--- form.js
+++ form.js
@@ -1,13 +1,21 @@
 // Selección de elementos
 const menuHamburguesa = document.querySelector('.menu-hamburguesa');
 const navLinks = document.querySelector('.nav-links');
+const navItems = document.querySelectorAll('.nav-links ul li a');
 
 // Evento para el menú hamburguesa
 menuHamburguesa.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-    menuHamburguesa.classList.toggle('open');
+    const isActive = navLinks.classList.toggle('active');
+    menuHamburguesa.classList.toggle('open', isActive);
 });
 
+// Evento para los elementos del menú
+navItems.forEach(item => {
+    item.addEventListener('click', () => {
+        navLinks.classList.remove('active'); // Cierra el menú
+        menuHamburguesa.classList.remove('open'); // Restablece el icono de hamburguesa
+    });
+});
 
 
 
